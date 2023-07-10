0b4c21c49676dbb7ac9d2bca107006f0ad0b436b
Noodieknoodie <eriklars08@gmail.com>
2023-07-10 12:24:51 -0700

    with new sample csv

--- src/utils/csvParser.js
+++ src/utils/csvParser.js
@@ -1,25 +1,107 @@
 import { parse } from "papaparse";
 
-let qualifiedKeywords = ['401(k)', '403(b)', 'IRA', 'IRA Intelligent', 'IRA Rollover', 'IRA Rollover 2', 'Roth Conversion', 'Roth Conversion IRA', 'Roth IRA', 'Roth IRA Intelligent', 'SEP IRA', 'Simple IRA', 'Inherited IRA', 'Inherited Roth IRA'];
-let taxFreeKeywords = ['529', 'Charitable', 'Education Savings', 'Utah 529', 'UTMA'];
-let nonQualifiedKeywords = ['Annuity', 'By-Pass Trust', 'Credit Shelter Trust', 'Family Trust', 'Grantor Trust', 'GRAT', 'GST', 'GST (Margin)', 'Irrevocable Trust', 'Living Trust', 'Living Trust (Bonds)', 'Living Trust (Dividend)', 'Living Trust (Equity)', 'Living Trust (Margin)', 'Living Trust (Yield)', 'Marital Trust', 'Pen Trust', 'Rev Trust 1', 'Rev Trust 2', 'Revocable Trust', 'Revocable Trust (Equity)', 'Revocable Trust (Yield)', 'Special Needs Trust', 'Supplemental Needs Trust', 'Survivors Trust', 'Testamentary Trust', 'Trust', 'Individual', 'Comm Prop WROS', 'Community Property', 'Community Property (Margin)', 'Community Property 2', 'Dsg Bene JT (Margin)', 'JT TEN', 'JT TEN - LINK', 'JT TEN - Travel', 'JT TEN (Fool Acct)', 'JT TEN (Income)', 'JT TEN (Investment)', 'JT TEN (Margin)', 'JT TEN (Tax)', 'JT TEN (Tech)', 'JT TEN (Trading)', 'JT TEN | Pledged', 'JT Ten 2', 'JT Ten Comp', 'JTWROS', 'JTWROS 4', 'JTWROS COMP', 'Ten Com', 'Ten Com (Margin)', 'Corporate', 'LLC', 'Partnership', 'Partnership (Margin)', 'Profit Sharing Plan', 'CRUT', 'Schwab Charitable', 'Annuity', 'Estate', 'OUTSIDE BILLING', 'Separate Property'];
+let qualifiedKeywords = [
+  "401(k)",
+  "403(b)",
+  "IRA",
+  "IRA Intelligent",
+  "IRA Rollover",
+  "IRA Rollover 2",
+  "Roth Conversion",
+  "Roth Conversion IRA",
+  "Roth IRA",
+  "Roth IRA Intelligent",
+  "SEP IRA",
+  "Simple IRA",
+  "Inherited IRA",
+  "Inherited Roth IRA",
+];
+let taxFreeKeywords = [
+  "529",
+  "Charitable",
+  "Education Savings",
+  "Utah 529",
+  "UTMA",
+];
+let nonQualifiedKeywords = [
+  "Annuity",
+  "By-Pass Trust",
+  "Credit Shelter Trust",
+  "Family Trust",
+  "Grantor Trust",
+  "GRAT",
+  "GST",
+  "GST (Margin)",
+  "Irrevocable Trust",
+  "Living Trust",
+  "Living Trust (Bonds)",
+  "Living Trust (Dividend)",
+  "Living Trust (Equity)",
+  "Living Trust (Margin)",
+  "Living Trust (Yield)",
+  "Marital Trust",
+  "Pen Trust",
+  "Rev Trust 1",
+  "Rev Trust 2",
+  "Revocable Trust",
+  "Revocable Trust (Equity)",
+  "Revocable Trust (Yield)",
+  "Special Needs Trust",
+  "Supplemental Needs Trust",
+  "Survivors Trust",
+  "Testamentary Trust",
+  "Trust",
+  "Individual",
+  "Comm Prop WROS",
+  "Community Property",
+  "Community Property (Margin)",
+  "Community Property 2",
+  "Dsg Bene JT (Margin)",
+  "JT TEN",
+  "JT TEN - LINK",
+  "JT TEN - Travel",
+  "JT TEN (Fool Acct)",
+  "JT TEN (Income)",
+  "JT TEN (Investment)",
+  "JT TEN (Margin)",
+  "JT TEN (Tax)",
+  "JT TEN (Tech)",
+  "JT TEN (Trading)",
+  "JT TEN | Pledged",
+  "JT Ten 2",
+  "JT Ten Comp",
+  "JTWROS",
+  "JTWROS 4",
+  "JTWROS COMP",
+  "Ten Com",
+  "Ten Com (Margin)",
+  "Corporate",
+  "LLC",
+  "Partnership",
+  "Partnership (Margin)",
+  "Profit Sharing Plan",
+  "CRUT",
+  "Schwab Charitable",
+  "Annuity",
+  "Estate",
+  "OUTSIDE BILLING",
+  "Separate Property",
+];
 
 function getAccountType(accountName) {
-  const lowerCaseAccountName = accountName.toLowerCase();
-
-  if (qualifiedKeywords.includes(lowerCaseAccountName)) {
-    return 'Qualified';
+  if (qualifiedKeywords.includes(accountName)) {
+    return "Qualified";
   }
 
-  if (taxFreeKeywords.includes(lowerCaseAccountName)) {
-    return 'Tax-Free';
+  if (taxFreeKeywords.includes(accountName)) {
+    return "Tax-Free";
   }
 
-  if (nonQualifiedKeywords.includes(lowerCaseAccountName)) {
-    return 'Non-Qualified';
+  if (nonQualifiedKeywords.includes(accountName)) {
+    return "Non-Qualified";
   }
 
-  return 'Unknown'; 
+  return "Unknown";
 }
 
 export function parseCSVData(csvData) {
@@ -29,14 +111,14 @@ export function parseCSVData(csvData) {
   });
 
   const accountTypeCounts = new Map([
-    ['Qualified', 0],
-    ['Non-Qualified', 0],
-    ['Tax-Free', 0]
+    ["Qualified", 0],
+    ["Non-Qualified", 0],
+    ["Tax-Free", 0],
   ]);
   const accountTypeSums = new Map([
-    ['Qualified', 0],
-    ['Non-Qualified', 0],
-    ['Tax-Free', 0]
+    ["Qualified", 0],
+    ["Non-Qualified", 0],
+    ["Tax-Free", 0],
   ]);
 
   const accountTypes = new Map();
@@ -78,10 +160,16 @@ export function parseCSVData(csvData) {
         id: accountId,
         name: accountName,
         value: accountValue,
-        type: accountType
+        type: accountType,
       });
-      accountTypeCounts.set(accountType, accountTypeCounts.get(accountType) + 1);
-      accountTypeSums.set(accountType, accountTypeSums.get(accountType) + accountValue);
+      accountTypeCounts.set(
+        accountType,
+        accountTypeCounts.get(accountType) + 1
+      );
+      accountTypeSums.set(
+        accountType,
+        accountTypeSums.get(accountType) + accountValue
+      );
       household.totalAccountValue += accountValue;
     }
 
@@ -89,10 +177,16 @@ export function parseCSVData(csvData) {
     return acc;
   }, {});
 
-  Object.values(summaryData).forEach(household => {
-    Array.from(household.accounts.values()).forEach(account => {
-      accountTypeCounts.set(account.type, accountTypeCounts.get(account.type) + 1);
-      accountTypeSums.set(account.type, accountTypeSums.get(account.type) + account.value);
+  Object.values(summaryData).forEach((household) => {
+    Array.from(household.accounts.values()).forEach((account) => {
+      accountTypeCounts.set(
+        account.type,
+        accountTypeCounts.get(account.type) + 1
+      );
+      accountTypeSums.set(
+        account.type,
+        accountTypeSums.get(account.type) + account.value
+      );
     });
   });
 
@@ -108,14 +202,14 @@ export function parseCSVData(csvData) {
       numberOfAccounts: household.accounts.size,
       totalAccountValue: household.totalAccountValue.toFixed(2),
       accountTypes: {
-        'Qualified': accountTypeCounts.get('Qualified'),
-        'Non-Qualified': accountTypeCounts.get('Non-Qualified'),
-        'Tax-Free': accountTypeCounts.get('Tax-Free')
+        Qualified: accountTypeCounts.get("Qualified"),
+        "Non-Qualified": accountTypeCounts.get("Non-Qualified"),
+        "Tax-Free": accountTypeCounts.get("Tax-Free"),
       },
       accountTypeSums: {
-        'Qualified': accountTypeSums.get('Qualified').toFixed(2),
-        'Non-Qualified': accountTypeSums.get('Non-Qualified').toFixed(2),
-        'Tax-Free': accountTypeSums.get('Tax-Free').toFixed(2)
+        Qualified: accountTypeSums.get("Qualified").toFixed(2),
+        "Non-Qualified": accountTypeSums.get("Non-Qualified").toFixed(2),
+        "Tax-Free": accountTypeSums.get("Tax-Free").toFixed(2),
       },
       currentAllocation: "TBD",
       targetAllocation: "TBD",
